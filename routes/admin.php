cab3255be2577b2d55b606ee7a1949363f277dc3
Exo <paink37@hotmail.com>
2023-06-29 16:42:49 +0100

    Created Slider Page

--- routes/admin.php
+++ routes/admin.php
@@ -4,8 +4,16 @@ use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
 
+/* Admin Routes */
+
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+/* Profile Routes */
 
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+/* Slider Route */
+
+Route::resource('slider', SliderController::class);
